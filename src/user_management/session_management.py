7c82d0c30c02dd7311e50ebba0bffac498125a4a
mmyuhhh <mayesha0905@gmail.com>
2023-10-17 18:56:17 -0230

    Implemented session_management.py

--- src/user_management/session_management.py
+++ src/user_management/session_management.py
@@ -1,5 +1,5 @@
 from src.data_management.object_mapper import ObjectMapper
-from src.app_logic.logic import Session
+from src.app_logic.app_logic import Session
 
 class SessionManager:
     def __init__(self, db_path: str):
@@ -10,8 +10,9 @@ class SessionManager:
             db_path (str): The path to the database where session data is stored.
         """
         self.db_path = db_path
-        self.sessions = []
-        
+        self.object_mapper = ObjectMapper(self.db_path)
+         
+
 
     def create_session(self, user_id: int, is_active: int = 1):
         """
@@ -25,7 +26,7 @@ class SessionManager:
             Session: The created session object.
         """
         session = Session(user_id, is_active)
-        self.sessions.append(session)  # Add the session to your sessions data structure
+        self.session.append(session)  
         return session
         
 
@@ -39,10 +40,9 @@ class SessionManager:
         Returns:
             Session: The session object if found, None otherwise.
         """
-        for session in self.sessions:
-            if session.session_id == session_id:
-                return session
-        return None
+        session = self.object_mapper.get(Session, id=session_id)
+        return session
+        
         
 
     def get_user_session(self, user_id: int):
@@ -55,8 +55,11 @@ class SessionManager:
         Returns:
             Session: The session object if found, None otherwise.
         """
-        for session in self.sessions:
-            if session.user_id == user_id:
+        all_sessions = self.object_mapper.get('Session')
+        for session_data in all_sessions:
+            if session_data['user_id'] == user_id:
+                # Create a Session object from the retrieved data
+                session = Session(**session_data)
                 return session
         return None
         
@@ -68,8 +71,4 @@ class SessionManager:
         Args:
             session (Session): The session object to update.
         """
-        for i, existing_session in enumerate(self.sessions):
-            if existing_session.session_id == session.session_id:
-                self.sessions[i] = session
-                return
-        
+        self.object_mapper.add(session)
