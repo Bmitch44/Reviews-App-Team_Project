0d0c23403f6936bb8c0ba86ad419b3782dbc1a90
mmyuhhh <mayesha0905@gmail.com>
2023-10-20 21:36:38 -0230

    fixed bugs

--- test/test_session_management.py
+++ test/test_session_management.py
@@ -10,34 +10,34 @@ class TestSessionManager(unittest.TestCase):
     def setUpClass(cls):
         cls.db_path = "test.db"
         cls.object_mapper = ObjectMapper(cls.db_path)
-        cls.session_manager = SessionManager(cls.db_path)
+        cls.session_management = SessionManager(cls.db_path)
 
     def setUp(self):
-        self.session_management.data_store.clear_tables()
-
+        self.object_mapper.data_store.clear_tables()
+         
     def test_create_session(self):
         user_id = 1
-        session = self.session_manager.create_session(user_id)
+        session = self.session_management.create_session(user_id)
         self.assertIsInstance(session, Session)
 
     def test_get_session(self):
         user_id = 1
-        session = self.session_manager.create_session(user_id)
-        retrieved_session = self.session_manager.get_session(session.id)
-        self.assertEqual(retrieved_session.id, session.id)
-
+        session = self.session_management.create_session(user_id)
+        retrieved_session = self.session_management.get_user_session(user_id)
+        self.assertIsNotNone(retrieved_session)
+ 
     def test_get_user_session(self):
         user_id = 1
-        session = self.session_manager.create_session(user_id)
-        retrieved_session = self.session_manager.get_user_session(user_id)
-        self.assertEqual(retrieved_session.id, session.id)
+        session = self.session_management.create_session(user_id)
+        retrieved_session = self.session_management.get_user_session(user_id)
+        self.assertEqual(retrieved_session.user_id, user_id)
 
     def test_update_session(self):
         user_id = 1
-        session = self.session_manager.create_session(user_id)
+        session = self.session_management.create_session(user_id)
         session.is_active = 0
-        self.session_manager.update_session(session)
-        retrieved_session = self.session_manager.get_session(session.id)
+        self.session_management.update_session(session)
+        retrieved_session = self.session_management.get_user_session(user_id)
         self.assertEqual(retrieved_session.is_active, 0)
 
     @classmethod
